8f1b902a3eec8c4d66667d92d00171d01af673c4
Namhoon Choe <33464899+namhoonchoe@users.noreply.github.com>
2024-08-12 11:43:22 +0900

    [Feat] Toggle sidebar
    사이드 바 토글 모드 추가
    grid-template-columns fit-content 참고

--- src/components/layouts/SidebarLayout/Sidebar.tsx
+++ src/components/layouts/SidebarLayout/Sidebar.tsx
@@ -3,33 +3,60 @@ import BoardsIcon from "@/components/svgIcons/BoardsIcon";
 import MemoIcon from "@/components/svgIcons/MemoIcon";
 import TimerIcon from "@/components/svgIcons/TimerIcon";
 import { Link } from "react-router-dom";
-export default function Sidebar() {
+export default function Sidebar({ isShrinked }: { isShrinked: boolean }) {
   return (
-    <section className="flex flex-col justify-start items-center w-56  gap-8 px-1.5 py-8 bg-zinc-900  sticky top-14 grid-side-bar  min-h-[calc(100vh-56px)] xl:h-[calc(100vh-56px)]  ">
-      <Link to="/">
-        <div className="nav-button  ">
-          <HomeIcon />
-          <p className=" subpixel-antialiased  font-medium">홈</p>
-        </div>
-      </Link>
-      <Link to="/boards">
-        <div className="nav-button   ">
-          <BoardsIcon />
-          <p className=" subpixel-antialiased  font-medium">프로젝트</p>
-        </div>
-      </Link>
-      <Link to="/memo">
-        <div className="nav-button  ">
-          <MemoIcon />
-          <p className=" subpixel-antialiased  font-medium">메모</p>
-        </div>
-      </Link>
-      <Link to="/pomodoro">
-        <div className="nav-button   ">
-          <TimerIcon />
-          <p className=" subpixel-antialiased  font-medium">집중 모드</p>
-        </div>
-      </Link>
-    </section>
+    <>
+      {isShrinked ? (
+        <section className="flex flex-col justify-start items-center w-16  min-h-[calc(100vh-56px)] xl:h-[calc(100vh-56px)] relative overflow-hidden gap-8 py-8 bg-zinc-900  ">
+          <Link to="/">
+            <div className="nav-button-shrinked  ">
+              <HomeIcon />
+            </div>
+          </Link>
+          <Link to="/boards">
+            <div className="nav-button-shrinked   ">
+              <BoardsIcon />
+            </div>
+          </Link>
+          <Link to="/memo">
+            <div className="nav-button-shrinked  ">
+              <MemoIcon />
+            </div>
+          </Link>
+          <Link to="/pomodoro">
+            <div className="nav-button-shrinked ">
+              <TimerIcon />
+            </div>
+          </Link>
+        </section>
+      ) : (
+        <section className="flex flex-col justify-start items-center w-56  gap-8 px-1.5 py-8 bg-zinc-900  sticky top-14 grid-side-bar  min-h-[calc(100vh-56px)] xl:h-[calc(100vh-56px)]  ">
+          <Link to="/">
+            <div className="nav-button  ">
+              <HomeIcon />
+              <p className=" subpixel-antialiased  font-medium">홈</p>
+            </div>
+          </Link>
+          <Link to="/boards">
+            <div className="nav-button   ">
+              <BoardsIcon />
+              <p className=" subpixel-antialiased  font-medium">프로젝트</p>
+            </div>
+          </Link>
+          <Link to="/memo">
+            <div className="nav-button  ">
+              <MemoIcon />
+              <p className=" subpixel-antialiased  font-medium">메모</p>
+            </div>
+          </Link>
+          <Link to="/pomodoro">
+            <div className="nav-button   ">
+              <TimerIcon />
+              <p className=" subpixel-antialiased  font-medium">집중 모드</p>
+            </div>
+          </Link>
+        </section>
+      )}
+    </>
   );
 }
--- src/components/layouts/SidebarLayout/SidebarLayout.tsx
+++ src/components/layouts/SidebarLayout/SidebarLayout.tsx
@@ -1,22 +1,66 @@
-import { ReactElement } from "react";
-import LayoutHeader from "./LayoutHeader";
+import { ReactElement, useState } from "react";
+import HambergerIcon from "@/components/svgIcons/HambergerIcon";
+import PersonIcon from "@/components/svgIcons/PersonIcon";
 import Sidebar from "./Sidebar";
 export default function SidebarLayout({
   children,
 }: {
   children: ReactElement;
 }) {
+  const [isShrinked, setisShrinked] = useState(false);
+
   return (
     <div className="fit-center">
-      <section className="layout-grid w-full relative">
+      <section
+        className={`${
+          isShrinked ? "layout-grid-shrinked" : "layout-grid"
+        } w-full relative`}
+      >
         {/* header */}
-        <LayoutHeader />
+        <header className="flex justify-between items-center grid-header w-full h-14 px-6 py-2 bg-zinc-900 ">
+          <button onClick={() => setisShrinked(!isShrinked)}>
+            <HambergerIcon />
+          </button>
+
+          <div className="flex justify-start items-center flex-grow-0 flex-shrink-0 w-[500px] relative overflow-hidden gap-2.5 px-4 py-3 rounded-3xl bg-zinc-700">
+            <svg
+              width={25}
+              height={24}
+              viewBox="0 0 25 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="flex-grow-0 flex-shrink-0 w-6 h-6"
+              preserveAspectRatio="none"
+            >
+              <mask
+                id="mask0_6_684"
+                style={{ maskType: "alpha" }}
+                maskUnits="userSpaceOnUse"
+                x={0}
+                y={0}
+                width={25}
+                height={24}
+              >
+                <rect x="0.0957031" width={24} height={24} fill="#D9D9D9" />
+              </mask>
+              <g mask="url(#mask0_6_684)">
+                <path
+                  d="M19.6957 21.0002L13.3957 14.7002C12.8957 15.1002 12.3207 15.4169 11.6707 15.6502C11.0207 15.8836 10.329 16.0002 9.5957 16.0002C7.77904 16.0002 6.24154 15.3711 4.9832 14.1127C3.72487 12.8544 3.0957 11.3169 3.0957 9.50024C3.0957 7.68358 3.72487 6.14608 4.9832 4.88774C6.24154 3.62941 7.77904 3.00024 9.5957 3.00024C11.4124 3.00024 12.9499 3.62941 14.2082 4.88774C15.4665 6.14608 16.0957 7.68358 16.0957 9.50024C16.0957 10.2336 15.979 10.9252 15.7457 11.5752C15.5124 12.2252 15.1957 12.8002 14.7957 13.3002L21.0957 19.6002L19.6957 21.0002ZM9.5957 14.0002C10.8457 14.0002 11.9082 13.5627 12.7832 12.6877C13.6582 11.8127 14.0957 10.7502 14.0957 9.50024C14.0957 8.25024 13.6582 7.18774 12.7832 6.31274C11.9082 5.43774 10.8457 5.00024 9.5957 5.00024C8.3457 5.00024 7.2832 5.43774 6.4082 6.31274C5.5332 7.18774 5.0957 8.25024 5.0957 9.50024C5.0957 10.7502 5.5332 11.8127 6.4082 12.6877C7.2832 13.5627 8.3457 14.0002 9.5957 14.0002Z"
+                  fill="white"
+                />
+              </g>
+            </svg>
+          </div>
+          <button>
+            <PersonIcon />
+          </button>
+        </header>
+
         {/* sidebar */}
-        <Sidebar />
+        <Sidebar isShrinked={isShrinked} />
+
         {/*main */}
-        <section className="h-full   grid-main  ">
-          {children}
-        </section>
+        <section className="h-full grid-main  ">{children}</section>
       </section>
     </div>
   );
