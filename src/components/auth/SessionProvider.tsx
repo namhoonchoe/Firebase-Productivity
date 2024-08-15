e0a01bac2a03f42913fbdd8bb5a5e8e29dd1dafd
Namhoon Choe <33464899+namhoonchoe@users.noreply.github.com>
2024-08-15 12:38:09 +0900

    [FIX] FireStore user
    기존 가입 여부와 관계 없이 setDoc가 호출되어 회원 정보를 초기화 했음
    로직 수정해서 문제 해결!

--- src/components/auth/SessionProvider.tsx
+++ src/components/auth/SessionProvider.tsx
@@ -1,7 +1,14 @@
 import { useState, useEffect, ReactElement } from "react";
 import { auth, db } from "@/services/firebase";
 import Loading from "../ui/Loading";
-import { setDoc, query, where, collection, doc,getDocs } from "firebase/firestore";
+import {
+  setDoc,
+  query,
+  where,
+  collection,
+  doc,
+  getDocs,
+} from "firebase/firestore";
 
 export default function SessionProvider({
   children,
@@ -9,29 +16,23 @@ export default function SessionProvider({
   children: ReactElement;
 }) {
   const [isLoading, setLoading] = useState(true);
-  const [isDocExist, setIsDocExist] = useState(false)
   const init = async () => {
     await auth.authStateReady();
     await auth.onAuthStateChanged(function (user) {
       /** create user document only for new user */
       if (user) {
-        const newUserId = user.uid;
+        const userId = user.uid;
         const usersRef = collection(db, "users");
-        const q = query(usersRef, where("user_id", "==", newUserId));
+        const q = query(usersRef, where("user_id", "==", userId));
 
         getDocs(q).then((querySnapShot) => {
-          console.log(querySnapShot.docs.length)
-          if(querySnapShot.docs.length == 1){
-            setIsDocExist(true)
+          if (querySnapShot.docs.length === 0) {
+             setDoc(doc(db, "users", user.uid), {
+              username: user.displayName,
+              user_id: user.uid,
+            });
           }
-         });
-        
-        if(isDocExist === false) {
-          setDoc(doc(db, "users", user.uid), {
-            username: user.displayName,
-            user_id: user.uid,
-          })
-        }
+        });
       }
     });
     setLoading(false);
@@ -41,7 +42,6 @@ export default function SessionProvider({
     init();
     return () => {
       // cleanup
-      setIsDocExist(false); 
     };
   }, []);
 
