d9f6656981257cc23f4c3703d535c3687b5369fd
Namhoon Choe <33464899+namhoonchoe@users.noreply.github.com>
2024-08-22 18:04:29 +0900

    [Feat] Draggable Card #2
    Draggable Card  높이 수정

--- src/pages/Board/DraggableCard.tsx
+++ src/pages/Board/DraggableCard.tsx
@@ -5,12 +5,10 @@ export default function DraggableCard({ cardId }: { cardId: string }) {
   const [task] = taskList.filter((task) => task.task_id === cardId);
   const { task_title } = task;
   return (
-    <div className="flex aspect-[3] w-64 flex-shrink-0 flex-grow-0 items-start justify-start gap-2.5 rounded-xl bg-zinc-700 px-6 py-4">
-      <div className="relative flex flex-shrink-0 flex-grow-0 items-center justify-start gap-2">
-        <p className="flex-shrink-0 flex-grow-0 text-left text-xl font-bold text-white">
-          {task_title}
-        </p>
-      </div>
+    <div className="flex   w-64 items-start justify-start  rounded-xl bg-zinc-700 px-6 py-4">
+      <p className="max-w-full text-xl font-bold text-white break-all">
+      {task_title}
+      </p>
     </div>
   );
 }
